854036c34b2eba92cbf284463fec5520d01f1628
Lord Meme <75008009+MisterMeme95@users.noreply.github.com>
2023-11-11 11:54:42 -0500

    Kotlin Project Set up
    I've added the new kotlin project. Gradle has been updated as has the Manifest.
    
    Lastly, the main function has an example of how to do the API calls.

--- app/src/main/java/com/example/feedfindr/MainActivity.kt
+++ app/src/main/java/com/example/feedfindr/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.feedfindr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestHeaders
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /* First we set the API url. Note, it requires the apiKey set at the top! */
+        val apiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=062d27ad37e442a7bcca3f349b183338&query=pasta&maxFat=25&number=2"
+        val client = AsyncHttpClient()
+
+        /* for codepath client library, the headers are just are Hashmaps.*/
+        val headers = RequestHeaders()
+
+        /*Set the map to this value. */
+        headers["Content-Type"] = "application/json"
+
+        /* This version of the get command includes the header.
+        *  I have temporarily set params to null. We'll need to create a params
+        *  hashmap so that we can add queries and what not to the url.
+        * */
+        client.get(apiUrl, headers, null, object: JsonHttpResponseHandler(){
+
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                val jsonObject = json.jsonObject
+                val resultsArray = jsonObject.getJSONArray("results")
+                Log.d("PRINTING RESULT ARRAY", resultsArray.toString())
+
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("ERROR TEXT", errorResponse)
+            }
+        })
+
     }
 }
