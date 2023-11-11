ed44faeb04e2ef1e82284e137e0efc55e35e28f2
Sanij Shrestha <saniz.sth123@gmail.com>
2023-11-11 14:22:59 -0500

    [added] query setup

--- app/src/main/java/com/example/feedfindr/MainActivity.kt
+++ app/src/main/java/com/example/feedfindr/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.feedfindr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import androidx.recyclerview.widget.RecyclerView
@@ -19,43 +20,53 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //initializing instance variables
         recyclerView = findViewById(R.id.results)
+
         var input = findViewById<EditText>(R.id.query)
-        query = input.toString()
 
-        /* First we set the API url. Note, it requires the apiKey set at the top! */
-        val apiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=062d27ad37e442a7bcca3f349b183338&query=apple&maxFat=25&number=4"
-        val client = AsyncHttpClient()
+        var button = findViewById<Button>(R.id.serachBtn)
 
-        /* for codepath client library, the headers are just are Hashmaps.*/
-        val headers = RequestHeaders()
-
-        /*Set the map to this value. */
-        headers["Content-Type"] = "application/json"
-
-        /* This version of the get command includes the header.
-        *  I have temporarily set params to null. We'll need to create a params
-        *  hashmap so that we can add queries and what not to the url.
-        * */
-        client.get(apiUrl, headers, null, object: JsonHttpResponseHandler(){
-
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                val jsonObject = json.jsonObject
-                val resultsArray = jsonObject.getJSONArray("results")
-                Log.d("PRINTING RESULT ARRAY", resultsArray.toString())
-
-            }
-
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                errorResponse: String,
-                throwable: Throwable?
-            ) {
-                Log.d("ERROR TEXT", errorResponse)
-            }
-        })
+        button.setOnClickListener {
+            query = input.text.toString()
+            fetchAPI()
+        }
 
     }
-}
+
+        private fun fetchAPI() {
+            /* First we set the API url. Note, it requires the apiKey set at the top! */
+            val apiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=062d27ad37e442a7bcca3f349b183338&query=${query}&maxFat=25&number=4"
+            val client = AsyncHttpClient()
+
+            /* for codepath client library, the headers are just are Hashmaps.*/
+            val headers = RequestHeaders()
+
+            /*Set the map to this value. */
+            headers["Content-Type"] = "application/json"
+
+            /* This version of the get command includes the header.
+            *  I have temporarily set params to null. We'll need to create a params
+            *  hashmap so that we can add queries and what not to the url.
+            * */
+            client.get(apiUrl, headers, null, object: JsonHttpResponseHandler(){
+
+                override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                    val jsonObject = json.jsonObject
+                    val resultsArray = jsonObject.getJSONArray("results")
+                    Log.d("PRINTING RESULT ARRAY", resultsArray.toString())
+
+                }
+
+                override fun onFailure(
+                    statusCode: Int,
+                    headers: Headers?,
+                    errorResponse: String,
+                    throwable: Throwable?
+                ) {
+                    Log.d("ERROR TEXT", errorResponse)
+                }
+            })
+        }
+    }
