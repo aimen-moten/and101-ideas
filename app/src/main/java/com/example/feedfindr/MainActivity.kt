36bc28c90a2f8118d456c368e610e3c1ed56d6f3
Sanij Shrestha <saniz.sth123@gmail.com>
2023-11-11 13:51:18 -0500

    [added]home page basic skeleton

--- app/src/main/java/com/example/feedfindr/MainActivity.kt
+++ app/src/main/java/com/example/feedfindr/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.feedfindr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import android.widget.RadioButton
+import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.RequestHeaders
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -12,10 +14,18 @@ import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
+    private var query = ""
+    private lateinit var recyclerView : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //initializing instance variables
+        recyclerView = findViewById(R.id.results)
+        var input = findViewById<EditText>(R.id.query)
+        query = input.toString()
+
         /* First we set the API url. Note, it requires the apiKey set at the top! */
-        val apiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=062d27ad37e442a7bcca3f349b183338&query=pasta&maxFat=25&number=2"
+        val apiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=062d27ad37e442a7bcca3f349b183338&query=apple&maxFat=25&number=4"
         val client = AsyncHttpClient()
 
         /* for codepath client library, the headers are just are Hashmaps.*/
